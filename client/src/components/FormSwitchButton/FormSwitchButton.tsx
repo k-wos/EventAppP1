c43fc13be0a198d69d6b0c50601e8bd35b1f4723
k-wos <kacper.wos33@gmail.com>
2023-05-23 20:54:51 +0200

    switcher add

--- client/src/components/FormSwitchButton/FormSwitchButton.tsx
+++ client/src/components/FormSwitchButton/FormSwitchButton.tsx
@@ -11,19 +11,33 @@ const FormSwitchButton: React.FC<FormSwitchButtonProps> = ({
   handleButtonClick,
 }) => {
   return (
-    <div className="form-buttons">
-      <button
-        className={`button ${activeButton === "login" ? "active" : ""}`}
-        onClick={() => handleButtonClick("login")}
-      >
-        Zaloguj się
-      </button>
-      <button
-        className={`button ${activeButton === "register" ? "active" : ""}`}
-        onClick={() => handleButtonClick("register")}
-      >
-        Załóż konto
-      </button>
+    // <div className="form-buttons">
+    //   <button
+    //     className={`button ${activeButton === "login" ? "active" : ""}`}
+    //     onClick={() => handleButtonClick("login")}
+    //   >
+    //     Zaloguj się
+    //   </button>
+    //   <button
+    //     className={`button ${activeButton === "register" ? "active" : ""}`}
+    //     onClick={() => handleButtonClick("register")}
+    //   >
+    //     Załóż konto
+    //   </button>
+    // </div>
+    <div>
+      <input className="check" type="checkbox" id="checkbox_toggle"></input>
+      <div className="checkbox">
+        <label htmlFor="checkbox_toggle" className="slide">
+          <label htmlFor="checkbox_toggle" className="toggle"></label>
+          <label htmlFor="checkbox_toggle" className="text">
+            Zaloguj się
+          </label>
+          <label htmlFor="checkbox_toggle" className="text">
+            Załóż konto
+          </label>
+        </label>
+      </div>
     </div>
   );
 };
