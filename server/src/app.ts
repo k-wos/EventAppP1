10e9a6848edcd8b258f6849535fa8ba0d18f1245
k-wos <kacper.wos33@gmail.com>
2023-06-17 17:38:16 +0200

    add try catch to get eventrs endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import EventModel from "./models/event";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World");
+app.get("/", async (req, res) => {
+  try {
+    const events = await EventModel.find().exec();
+    res.status(200).json(events);
+  } catch (error) {
+    console.error(error);
+    let errorMassage = "Nieznany błąd";
+    if (error instanceof Error) errorMassage = error.message;
+    res.status(500);
+  }
 });
 
 export default app;
