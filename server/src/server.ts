77a3d9d57ebf089ed6faf6bb31a50a2ae6d41c89
k-wos <kacper.wos33@gmail.com>
2023-06-17 17:22:48 +0200

    create app file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,11 @@
-import "dotenv/config";
+import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
-import express from "express";
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World");
-});
-
-const port = process.env.PORT;
+const port = env.PORT;
 
 mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log("Mongoose conntected");
     app.listen(port, () => {
