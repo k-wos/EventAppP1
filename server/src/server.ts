1d4473085c3ffe552237fd17342054d79435d19f
k-wos <kacper.wos33@gmail.com>
2023-06-17 16:17:01 +0200

    add mongoose conection and install envalid

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,20 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, World");
+  res.send("Hello, World");
 });
 
-app.listen(port, ()=>{
-    console.log("Server running on port: " + port);
-})
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Mongoose conntected");
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+    });
+  })
+  .catch(console.error);
