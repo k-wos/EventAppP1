378051b0c24047f2b98023f20502a7620fe1fbf0
k-wos <kacper.wos33@gmail.com>
2023-06-19 15:08:21 +0200

    add eventForm and install reactr hook form

--- client/src/components/AddEventDialog.tsx
+++ client/src/components/AddEventDialog.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 interface AddEventDialogProps {
   onDismiss: () => void;
@@ -11,6 +11,26 @@ export const AddEventDialog = ({ onDismiss }: AddEventDialogProps) => {
       <Modal.Header closeButton>
         <Modal.Title>Dodaj Wydarzenie</Modal.Title>
       </Modal.Header>
+
+      <Modal.Body>
+        <Form id="addEventForm">
+          <Form.Group className="mb-3">
+            <Form.Label>Nazwa wydarzenia</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Opis</Form.Label>
+            <Form.Control as="textarea" rows={5} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button type="submit" form="addEventForm">
+          {" "}
+          Dodaj
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
